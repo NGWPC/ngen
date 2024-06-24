4ce5b94de5ba9c44aea68cc7d9d3456cea0037f9
Christina.Osumi <christina.osumi@rtx.com>
2024-06-24 15:19:52 -0700

    Fixed paths

--- scripts/submodule_update.sh
+++ scripts/submodule_update.sh
@@ -2,9 +2,9 @@
 
 # Clone ngen submodules
 #git submodule update --init --recursive
-git submodule add --force https://gitlab.sh.netgenwaterprediction/NGWPC/nwm-ngen/lgar-c.git extern/LASAM/
-git submodule add --force https://gitlab.sh.netgenwaterprediction/NGWPC/nwm-ngen/snow17.git extern/snow17/
-git submodule add --force https://gitlab.sh.netgenwaterprediction/NGWPC/nwm-ngen/sac-sma.git ./extern/sac-sma/sac-sma/
+git submodule add --force https://gitlab.sh.nextgenwaterprediction.com/NGWPC/nwm-ngen/lgar-c.git extern/LASAM/
+git submodule add --force https://gitlab.sh.nextgenwaterprediction.com/NGWPC/nwm-ngen/snow17.git extern/snow17/
+git submodule add --force https://gitlab.sh.nextgenwaterprediction.com/NGWPC/nwm-ngen/sac-sma.git ./extern/sac-sma/sac-sma/
 git submodule update --init --recursive
 cp ./extern/sac-sma/sac-sma/ngen_files/sacbmi.pc.in ./extern/sac-sma/sac-sma/ngen_files/CMakeLists.txt ./extern/sac-sma
 git submodule update --remote extern/cfe/cfe
