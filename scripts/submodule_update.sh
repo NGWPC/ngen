e1800da6fd065bfa03dc7b2722630a1c4a6dd9dd
Christina Osumi <christina.osumi@rtx.com>
2024-06-26 21:04:57 +0000

    Update submodule_update.sh

--- scripts/submodule_update.sh
+++ scripts/submodule_update.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# Clone ngen submodules
-#git submodule update --init --recursive
+git config --global url."https://oauth2:${GITLAB_TOKEN}@gitlab.sh.nextgenwaterprediction.com/".insteadOf "https://gitlab.sh.nextgenwaterprediction.com/"
+git submodule sync --recursive
+git submodule update --init --remote --recursive
+
 git submodule add --force https://gitlab.sh.nextgenwaterprediction.com/NGWPC/nwm-ngen/lgar-c.git extern/LASAM/
+git -C extern/LASAM pull
 git submodule add --force https://gitlab.sh.nextgenwaterprediction.com/NGWPC/nwm-ngen/snow17.git extern/snow17/
 git submodule add --force https://gitlab.sh.nextgenwaterprediction.com/NGWPC/nwm-ngen/sac-sma.git ./extern/sac-sma/sac-sma/
-git submodule update --init --recursive
+
 cp ./extern/sac-sma/sac-sma/ngen_files/sacbmi.pc.in ./extern/sac-sma/sac-sma/ngen_files/CMakeLists.txt ./extern/sac-sma
-git submodule update --remote extern/cfe/cfe
-git submodule update --remote extern/SoilFreezeThaw/SoilFreezeThaw
-git submodule update --remote extern/SoilMoistureProfiles/SoilMoistureProfiles
+
 git submodule set-branch --branch master extern/t-route
 git submodule update --remote extern/t-route
-git submodule set-branch --branch latest extern/sloth/
-git submodule update --remote extern/sloth/
