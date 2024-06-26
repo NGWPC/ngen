4404fd72b4db9f5bfd899f7e355c374bd6a3f8f6
Christina Osumi <christina.osumi@rtx.com>
2024-06-26 22:26:38 +0000

    Update submodule_update.sh

--- scripts/submodule_update.sh
+++ scripts/submodule_update.sh
@@ -2,14 +2,21 @@
 
 git config --global url."https://oauth2:${GITLAB_TOKEN}@gitlab.sh.nextgenwaterprediction.com/".insteadOf "https://gitlab.sh.nextgenwaterprediction.com/"
 git submodule sync --recursive
-git submodule update --init --remote --recursive
+git fetch --recurse-submodules
 
 git submodule add --force https://gitlab.sh.nextgenwaterprediction.com/NGWPC/nwm-ngen/lgar-c.git extern/LASAM/
-git -C extern/LASAM pull
 git submodule add --force https://gitlab.sh.nextgenwaterprediction.com/NGWPC/nwm-ngen/snow17.git extern/snow17/
 git submodule add --force https://gitlab.sh.nextgenwaterprediction.com/NGWPC/nwm-ngen/sac-sma.git ./extern/sac-sma/sac-sma/
 
 cp ./extern/sac-sma/sac-sma/ngen_files/sacbmi.pc.in ./extern/sac-sma/sac-sma/ngen_files/CMakeLists.txt ./extern/sac-sma
 
+git submodule update --remote extern/SoilFreezeThaw/SoilFreezeThaw
+git submodule update --remote extern/SoilMoistureProfiles/SoilMoistureProfiles
+git submodule update --remote extern/cfe/cfe
+git submodule update --remote extern/evapotranspiration/evapotranspiration
+git submodule update --remote extern/noah-owp-modular/noah-owp-modular
+git submodule update --remote extern/topmodel/topmodel
 git submodule set-branch --branch master extern/t-route
 git submodule update --remote extern/t-route
+git submodule set-branch --branch latest extern/sloth/
+git submodule update --remote extern/sloth/
