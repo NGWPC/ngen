1cd8183929a0d37e08456971b95d139f67ac6cba
Christina Osumi <christina.osumi@rtx.com>
2024-06-27 19:45:43 +0000

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,6 @@ export CXX=/bin/g++
 export BOOST_ROOT=/ngen-app/opt/boost_1_79_0
 export PATH="$PATH:/usr/lib64/openmpi/bin"
 source /opt/ngen/python/bin/activate
-pip3 install bmipy numpy==1.26.4 pyyaml pandas netCDF4==1.6.3
 
 # cmake_build
 cmake -B cmake_build -S . -DNGEN_WITH_MPI=ON -DNGEN_WITH_NETCDF=ON -DNGEN_WITH_SQLITE=ON -DNGEN_WITH_UDUNITS=ON -DNGEN_WITH_BMI_FORTRAN=ON -DNGEN_WITH_BMI_C=ON -DNGEN_WITH_PYTHON=ON -DNGEN_WITH_TESTS=ON -DNGEN_WITH_ROUTING=ON -DNGEN_QUIET=ON
@@ -14,32 +13,32 @@ cmake --build cmake_build --target all
 ./cmake_build/ngen
 
 # cmake_build_no_mpi
-cmake -B cmake_build_no_mpi -S . -DNGEN_WITH_MPI=OFF -DNGEN_WITH_NETCDF=ON -DNGEN_WITH_SQLITE=ON -DNGEN_WITH_UDUNITS=ON -DNGEN_WITH_BMI_FORTRAN=ON -DNGEN_WITH_BMI_C=ON -DNGEN_WITH_PYTHON=ON -DNGEN_WITH_TESTS=ON -DNGEN_WITH_ROUTING=ON -DNGEN_QUIET=ON
-cmake --build cmake_build_no_mpi --target all
+#cmake -B cmake_build_no_mpi -S . -DNGEN_WITH_MPI=OFF -DNGEN_WITH_NETCDF=ON -DNGEN_WITH_SQLITE=ON -DNGEN_WITH_UDUNITS=ON -DNGEN_WITH_BMI_FORTRAN=ON -DNGEN_WITH_BMI_C=ON -DNGEN_WITH_PYTHON=ON -DNGEN_WITH_TESTS=ON -DNGEN_WITH_ROUTING=ON -DNGEN_QUIET=ON
+#cmake --build cmake_build_no_mpi --target all
 
 # verify build
-./cmake_build_no_mpi/ngen
+#./cmake_build_no_mpi/ngen
 
 # cmake_build_no_netcdf
-cmake -B cmake_build_no_netcdf -S . -DNGEN_WITH_MPI=ON -DNGEN_WITH_NETCDF=OFF -DNGEN_WITH_SQLITE=ON -DNGEN_WITH_UDUNITS=ON -DNGEN_WITH_BMI_FORTRAN=ON -DNGEN_WITH_BMI_C=ON -DNGEN_WITH_PYTHON=ON -DNGEN_WITH_TESTS=ON -DNGEN_WITH_ROUTING=ON -DNGEN_QUIET=ON
-cmake --build cmake_build_no_netcdf --target all
+#cmake -B cmake_build_no_netcdf -S . -DNGEN_WITH_MPI=ON -DNGEN_WITH_NETCDF=OFF -DNGEN_WITH_SQLITE=ON -DNGEN_WITH_UDUNITS=ON -DNGEN_WITH_BMI_FORTRAN=ON -DNGEN_WITH_BMI_C=ON -DNGEN_WITH_PYTHON=ON -DNGEN_WITH_TESTS=ON -DNGEN_WITH_ROUTING=ON -DNGEN_QUIET=ON
+#cmake --build cmake_build_no_netcdf --target all
 
 # verify build
-./cmake_build_no_netcdf/ngen
+#./cmake_build_no_netcdf/ngen
 
 # cmake_build_no_mpi_netcdf
-cmake -B cmake_build_no_mpi_netcdf -S . -DNGEN_WITH_MPI=OFF -DNGEN_WITH_NETCDF=OFF -DNGEN_WITH_SQLITE=ON -DNGEN_WITH_UDUNITS=ON -DNGEN_WITH_BMI_FORTRAN=ON -DNGEN_WITH_BMI_C=ON -DNGEN_WITH_PYTHON=ON -DNGEN_WITH_TESTS=ON -DNGEN_WITH_ROUTING=ON -DNGEN_QUIET=ON
-cmake --build cmake_build_no_mpi_netcdf --target all
+#cmake -B cmake_build_no_mpi_netcdf -S . -DNGEN_WITH_MPI=OFF -DNGEN_WITH_NETCDF=OFF -DNGEN_WITH_SQLITE=ON -DNGEN_WITH_UDUNITS=ON -DNGEN_WITH_BMI_FORTRAN=ON -DNGEN_WITH_BMI_C=ON -DNGEN_WITH_PYTHON=ON -DNGEN_WITH_TESTS=ON -DNGEN_WITH_ROUTING=ON -DNGEN_QUIET=ON
+#cmake --build cmake_build_no_mpi_netcdf --target all
 
 # verify build
-./cmake_build_no_mpi_netcdf/ngen
+#./cmake_build_no_mpi_netcdf/ngen
 
 # cmake_build_no_sqlite
-cmake -B cmake_build_no_sqlite -S . -DNGEN_WITH_MPI=ON -DNGEN_WITH_NETCDF=ON -DNGEN_WITH_SQLITE=OFF -DNGEN_WITH_UDUNITS=ON -DNGEN_WITH_BMI_FORTRAN=ON -DNGEN_WITH_BMI_C=ON -DNGEN_WITH_PYTHON=ON -DNGEN_WITH_TESTS=ON -DNGEN_WITH_ROUTING=ON -DNGEN_QUIET=ON
-cmake --build cmake_build_no_sqlite --target all
+#cmake -B cmake_build_no_sqlite -S . -DNGEN_WITH_MPI=ON -DNGEN_WITH_NETCDF=ON -DNGEN_WITH_SQLITE=OFF -DNGEN_WITH_UDUNITS=ON -DNGEN_WITH_BMI_FORTRAN=ON -DNGEN_WITH_BMI_C=ON -DNGEN_WITH_PYTHON=ON -DNGEN_WITH_TESTS=ON -DNGEN_WITH_ROUTING=ON -DNGEN_QUIET=ON
+#cmake --build cmake_build_no_sqlite --target all
 
 # verify build
-./cmake_build_no_sqlite/ngen
+#./cmake_build_no_sqlite/ngen
 
 # Build ngen submodules
 cmake -B extern/LASAM/cmake_build -S extern/LASAM/ -DNGEN=ON
@@ -56,6 +55,9 @@ make -C extern/SoilMoistureProfiles/cmake_build/
 
 cmake -B extern/SoilFreezeThaw/cmake_build -S extern/SoilFreezeThaw/SoilFreezeThaw/ -DNGEN=ON
 make -C extern/SoilFreezeThaw//cmake_build/
+
+cmake -B extern/cfe/cmake_build -S extern/cfe/
+make -C extern/cfe/cmake_build/
 
 cd extern/t-route/
 ./compiler.sh no-e
