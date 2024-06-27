7f6d7c30a0a97fc8200fd4c07e19b5812d516e96
Christina Osumi <christina.osumi@rtx.com>
2024-06-27 19:36:20 +0000

    Update submodule_update.sh

--- scripts/submodule_update.sh
+++ scripts/submodule_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git config --global url."https://oauth2:${GITLAB_TOKEN}@gitlab.sh.nextgenwaterprediction.com/".insteadOf "https://gitlab.sh.nextgenwaterprediction.com/"
-git submodule sync --recursive
+#git config --global url."https://oauth2:${GITLAB_TOKEN}@gitlab.sh.nextgenwaterprediction.com/".insteadOf "https://gitlab.sh.nextgenwaterprediction.com/"
+#git submodule sync --recursive
 
 git submodule add --force https://gitlab.sh.nextgenwaterprediction.com/NGWPC/nwm-ngen/lgar-c.git extern/LASAM/
 git submodule add --force https://gitlab.sh.nextgenwaterprediction.com/NGWPC/nwm-ngen/snow17.git extern/snow17/
@@ -15,7 +15,7 @@ git submodule update --remote extern/cfe/cfe
 git submodule update --remote extern/evapotranspiration/evapotranspiration
 git submodule update --remote extern/noah-owp-modular/noah-owp-modular
 git submodule update --remote extern/topmodel/topmodel
-git submodule set-branch --branch master extern/t-route
+git submodule set-branch --branch development extern/t-route
 git submodule update --remote extern/t-route
-git submodule set-branch --branch latest extern/sloth/
+git submodule set-branch --branch development extern/sloth/
 git submodule update --remote extern/sloth/
